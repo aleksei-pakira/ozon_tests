f48210e584f290efe88259236dc8158888ebae59
alexey evlanov <lanceleelasparrow@gmail.com>
2024-07-01 13:25:52 +0400

    test remote 1

--- ozon_test_project/pages/ozon_travel_open.py
+++ ozon_test_project/pages/ozon_travel_open.py
@@ -9,8 +9,8 @@ class TravelPage:
         time.sleep(5)
 
     def open_ozon_travel_page(self):
-        browser.element(by.link_text('Билеты, отели, туры')).click()
-#        browser.element(by.partial_link_text('a[href="https://www.ozon.ru/travel/?mwc_campaign=oztravel_horizontal-menu_flight"]')).click()
+#        browser.element(by.link_text('Билеты, отели, туры')).click()
+        browser.element(by.partial_link_text('a[href="https://www.ozon.ru/travel/?mwc_campaign=oztravel_horizontal-menu_flight"]')).click()
         time.sleep(5)
 
     def open_tours_page(self):
